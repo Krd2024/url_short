c1c45ccb69beeec0eae4c3f77da5e8b699089e2f
Krd2024 <polzov.ya@ya.ru>
2025-02-17 19:52:08 +0300

    Убирает лишние библиотеки и добавляет новые

--- url/urls.py
+++ url/urls.py
@@ -4,7 +4,6 @@ from .views import index, shorten_url, url_delete, url_short_get
 urlpatterns = [
     path("", index, name="index"),
     path("url_short/", shorten_url, name="new_url_db"),
-    path("my_view/", fetch_and_parse_page, name="new_umy_viewrl_db"),
     path("url_short_get/", url_short_get, name="url_short_get"),
     path("api/urls/<int:id>/", url_delete, name="url_delete"),
 ]
